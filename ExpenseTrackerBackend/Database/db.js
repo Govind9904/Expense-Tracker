bdcbc28720c377dc9766327bbcb7b353c567447f
Govind Prajapati <prajapatigovind2210@gmail.com>
2026-07-21 18:55:24 +0530

    Configure cloud database connection

--- ExpenseTrackerBackend/Database/db.js
+++ ExpenseTrackerBackend/Database/db.js
@@ -1,13 +1,30 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("ExpenseTracker", "root", "1234", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST, // This will now correctly pull the full URL from your .env
+    port: process.env.DB_PORT || 21386,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false, // Keeps the required secure Aiven connection active
+      },
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+);
 
 sequelize
   .authenticate()
-  .then(() => console.log("MySQL Connected successfully"))
+  .then(() => console.log("MySQL Cloud Connected successfully!"))
   .catch((err) => console.log("DB Error:", err));
 
 module.exports = sequelize;
